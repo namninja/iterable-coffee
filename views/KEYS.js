695a2c961a6bc3807f7cf3b958849da9b7be4f97
Nam Ninja <namninja@gmail.com>
2023-03-01 13:52:19 -0800

    update

--- views/KEYS.js
+++
@@ -1,7 +0,0 @@
-//Replace the values in quotes and save
-
-window.SEGMENT_API_KEY = 'qD0k4dfDDBwocsA9I4GoYjmaTOMtqOfu'
-window.ITERABLE_WEB_IDENTIFIER = 'com.namngo.webinapptest'
-window.ITERABLE_EMAIL = 'nam.ngo+web@iterable.com'
-window.ITERABLE_API_KEY = '9817effb62d1435e9d4c54a65eaf2f7d'
-window.ITERABLE_JWT_SECRET = '9e281091a6916184f0f4e719a4ac8fecb1b00d486640da22b15120c3188695d6307467e06614bee84c10352e65b622ef83e183f819d1c36feb4d9d732134f39f'
