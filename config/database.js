3c07cef7c2fdd0117f306c0cdb506aa59cf20b64
Nam Ninja <namninja@gmail.com>
2023-03-01 14:40:11 -0800

    updating resources

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
-  "mongodb+srv://zero-admin:WutangClan@cluster0-pfvwv.mongodb.net/iterable-coffee?retryWrites=true";
+  "mongodb+srv://zero-admin:WutangClan@cluster0.pfvwv.mongodb.net/iterable-coffee?retryWrites=true";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
-  "mongodb+srv://zero-admin:WutangClan@cluster0-pfvwv.mongodb.net/test-iterable-coffee?retryWrites=true";
+  "mongodb+srv://zero-admin:WutangClan@cluster0.pfvwv.mongodb.net/test-iterable-coffee?retryWrites=true";
 
 exports.PORT = process.env.PORT || 80;
