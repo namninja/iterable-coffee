587bac5b98e80a9665dd0570903ab21c4ae85ef7
Nam Ninja <namninja@gmail.com>
2024-02-29 10:52:45 -0800

    update

--- config/database.js
+++ config/database.js
@@ -12,3 +12,6 @@ exports.PORT = process.env.PORT || 80;
 exports.SECRET = "f29c0de6f2d25e45f8eaa4fd3b933fa255b84d0950f97da0bb8689fc7fa1482b5028595f89bad06d2d5cc21270bc4b6145d7e22134131a88d982dc5de6c4f5fd"
 
 exports.ITKEY = "4c325b5acb4b4754b15e5d4456c9418d"
+
+
+
